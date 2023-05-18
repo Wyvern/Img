8cf3ba17bb5461efb8b0ab30a0d4e4083fece7ab
Larry <git@hub.com>
2023-05-18 17:01:04 +0800

    add new sites support.

--- src/main.rs
+++ src/main.rs
@@ -438,7 +438,7 @@ mod tests {
 
     #[test]
     fn try_it() {
-        let addr = "https://www.rosi8.com/pic/46310_43.html";
+        let addr = "https://www.shxiaowu.com/plus/view-774340-60.html";
         parse(addr);
     }
 
